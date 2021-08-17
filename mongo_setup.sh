96a3b1c63511bb8f4dfe1dc0ad2b3b21ab3a4061
nishi2go <nishi2go@gmail.com>
2021-08-17 11:57:51 +0900

    Update the mongodb setup script to the latest level

--- mongo_setup.sh
+++ mongo_setup.sh
@@ -1,107 +1,69 @@
 #!/bin/bash
 
 ## This Script installs mongo operator for MAS.
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
+SCRIPT_DIR=$(
+  cd $(dirname $0)
+  pwd
+)
 
 source "${SCRIPT_DIR}/behavior-analytics-services/Installation Scripts/bas-script-functions.bash"
 
 function stepLog() {
-  echo -e "STEP $1/6: $2"
+  echo -e "STEP $1/3: $2"
 }
 
 if [ -z "$MONGO_NAMESPACE" ]; then
-  MONGO_NAMESPACE="mongo"
+  export MONGO_NAMESPACE="mongo"
+fi
+
+if [ -z "$MONGODB_STORAGE_CLASS" ]; then
+  export MONGODB_STORAGE_CLASS="local-path"
+fi
+
+if [ -z "$MONGOD_STORAGE_GB" ]; then
+  export MONGOD_STORAGE_GB="5Gi"
+fi
+
+if [ -z "$MONGOD_STORAGE_LOGS_GB" ]; then
+  export MONGOD_STORAGE_LOGS_GB="500Mi"
+fi
+
+if [ -z "${MONGO_PASSWORD}" ]; then
+  export MONGO_PASSWORD=$(openssl rand -hex 10)
+fi
+
+if [ -z "${MONGOD_REPLICAS}" ]; then
+  export MONGOD_REPLICAS=1
 fi
 
 mkdir -p logs
 WORK_DIR="${SCRIPT_DIR}/work"
-DATETIME=`date +%Y%m%d_%H%M%S`
+DATETIME=$(date +%Y%m%d_%H%M%S)
 
 logFile="${SCRIPT_DIR}/logs/mongo-installation-${DATETIME}.log"
 touch "${logFile}"
 
 status=$(oc whoami 2>&1)
 if [[ $? -gt 0 ]]; then
-    echoRed "Login to OpenShift to continue MongoDB Operator installation."
-        exit 1;
+  echoRed "Login to OpenShift to continue MongoDB Operator installation."
+  exit 1
 fi
 
-displayStepHeader 1 "Create namespace for MongoDB"
-projectName=${MONGO_NAMESPACE}
-createProject
-
-displayStepHeader 2 "Copy necessary files from MongoDB CE Operator."
-mkdir -p "${WORK_DIR}"
+displayStepHeader 1 "Prepare setup scripts for MongoDB CE Operator."
+mkdir -p "${WORK_DIR}/mongodb"
+rm -rf "${WORK_DIR}/mongodb/*"
 cp -r "${SCRIPT_DIR}/iot-docs/mongodb" "${WORK_DIR}"
+sed -i.bak "s|cpu: 500m|cpu: 10m|g" "${WORK_DIR}/mongodb/config/manager/__manager__.yaml"
+sed -i.bak "s|members: 3|members: ${MONGOD_REPLICAS}|g" "${WORK_DIR}/mongodb/config/mas-mongo-ce/__mas_v1_mongodbcommunity_openshift_cr__.yaml"
+sed -i.bak "s|!= \"3\"|!= \"${MONGOD_REPLICAS}\"|g" "${WORK_DIR}/mongodb/install-mongo-ce.sh"
 
-mkdir -p "${WORK_DIR}/mongodb/config/rbac"
-cp "${SCRIPT_DIR}/mongodb-kubernetes-operator/config/rbac/kustomization.yaml" "${WORK_DIR}/mongodb/config/rbac/"
-cp "${SCRIPT_DIR}/mongodb-kubernetes-operator/config/rbac/role_binding.yaml" "${WORK_DIR}/mongodb/config/rbac/"
-cp "${SCRIPT_DIR}/mongodb-kubernetes-operator/config/rbac/role.yaml" "${WORK_DIR}/mongodb/config/rbac/"
-cp "${SCRIPT_DIR}/mongodb-kubernetes-operator/config/rbac/service_account.yaml" "${WORK_DIR}/mongodb/config/rbac/"
-
-mkdir -p "${WORK_DIR}/mongodb/config/manager"
-cp "${SCRIPT_DIR}/mongodb-kubernetes-operator/config/manager/manager.yaml" "${WORK_DIR}/mongodb/config/manager/"
-
-mkdir -p "${WORK_DIR}/mongodb/config/crd"
-cp "${SCRIPT_DIR}/mongodb-kubernetes-operator/config/crd/bases/mongodbcommunity.mongodb.com_mongodbcommunity.yaml" "${WORK_DIR}/mongodb/config/crd/"
-
-displayStepHeader 3 "Generate self-signed certificates."
+displayStepHeader 2 "Generate self-signed certificates."
 
 cd "${WORK_DIR}/mongodb/certs"
 bash "${WORK_DIR}/mongodb/certs/generateSelfSignedCert.sh" | tee -a tee "${logFile}"
 
-displayStepHeader 4 "Update MongoDB password."
-
-if [ -z "${MONGO_PASSWORD}" ]; then
-  MONGO_PASSWORD=`openssl rand -hex 10`
-fi
-
-cat <<EOF | oc apply -f - | tee -a tee "${logFile}"
-apiVersion: v1
-kind: Secret
-metadata:
-  name: mas-mongo-ce-admin-password
-  namespace: ${MONGO_NAMESPACE}
-type: Opaque
-stringData:
-  password: ${MONGO_PASSWORD}
-EOF
-
-displayStepHeader 5 "Install MongoDB Operator."
-cp "${SCRIPT_DIR}/mongodb/msi-mas_v1_mongodbcommunity_openshift_cr.yaml" "${WORK_DIR}/mongodb/config/mas-mongo-ce/mas_v1_mongodbcommunity_openshift_cr.yaml"
+displayStepHeader 3 "Install MongoDB Operator."
 
 cd "${WORK_DIR}/mongodb/"
 
-oc new-project ${MONGO_NAMESPACE}
-
-oc apply -f config/crd/mongodbcommunity.mongodb.com_mongodbcommunity.yaml -n ${MONGO_NAMESPACE}
-
-oc apply -k config/rbac/.  -n ${MONGO_NAMESPACE}
-
-oc adm policy add-scc-to-user anyuid system:serviceaccount:${MONGO_NAMESPACE}:default
-oc adm policy add-scc-to-user anyuid system:serviceaccount:${MONGO_NAMESPACE}:mongodb-kubernetes-operator
-
-oc create -f config/manager/manager.yaml -n ${MONGO_NAMESPACE}
-echo -n " - Waiting for MongoDB CE Operator  "
-while [[ $(oc get deployment mongodb-kubernetes-operator -n ${MONGO_NAMESPACE} -o 'jsonpath={..status.conditions[?(@.type=="Available")].status}') != "True" ]];do sleep 5s; done
-
-cd certs
-oc create configmap mas-mongo-ce-cert-map --from-file=ca.crt=ca.pem -n ${MONGO_NAMESPACE}
-oc create secret tls mas-mongo-ce-cert-secret --cert=server.crt --key=server.key -n ${MONGO_NAMESPACE}
-cd ..
-
-oc apply -f config/mas-mongo-ce/mas_v1_mongodbcommunity_openshift_cr.yaml -n ${MONGO_NAMESPACE}
-sleep 5s
-while [[ $(oc get statefulset mas-mongo-ce -n ${MONGO_NAMESPACE} --ignore-not-found -o 'jsonpath={..status.readyReplicas}') != "1" ]];do sleep 5s; done
-
-oc rollout restart statefulset mas-mongo-ce -n ${MONGO_NAMESPACE}
-sleep 5s
-
-displayStepHeader 6 "Enable SCRAM-SHA-1 Auth."
-JSON=$(oc get secret mas-mongo-ce-config -n ${MONGO_NAMESPACE} -o 'jsonpath={..data.cluster-config\.json}' | base64 -d  | jq -c '.auth.autoAuthMechanisms|=["SCRAM-SHA-256", "SCRAM-SHA-1"]' | jq -c '.auth.deploymentAuthMechanisms|=["SCRAM-SHA-256", "SCRAM-SHA-1"]' | base64 -w 0)
-
-oc patch secret mas-mongo-ce-config -n "${MONGO_NAMESPACE}" -p="{\"data\":{\"cluster-config.json\": \"${JSON}\"}}" -v=1
-oc rollout restart statefulset mas-mongo-ce -n "${MONGO_NAMESPACE}"
-sleep 10s
-while [[ $(oc get statefulset mas-mongo-ce -n ${MONGO_NAMESPACE} --ignore-not-found -o 'jsonpath={..status.readyReplicas}') != "1" ]];do sleep 5s; done
+bash ./install-mongo-ce.sh
