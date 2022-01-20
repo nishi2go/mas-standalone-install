89b0c5dc1b360e6d61a33939eb91c9c13f17e505
nishi2go <nishi2go@gmail.com>
2022-01-20 16:44:27 +0900

    Remove inquiry when project is already available

--- util.sh
+++ util.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
 
 function checkOperatorInstallationSucceeded() {
-	retryCount=120
-	retries=0
-	check_for_csv_success=$(oc get csv -n "$projectName" --ignore-not-found | grep  --color=never "${operatorName}" | awk -F' ' '{print $NF}')
-	until [[ $retries -eq $retryCount || $check_for_csv_success = "Succeeded" ]]; do
-		sleep 5
-		check_for_csv_success=$(oc get csv -n "$projectName" --ignore-not-found | grep  --color=never "${operatorName}" | awk -F' ' '{print $NF}')
-		retries=$((retries + 1))
-	done
-	echo "$check_for_csv_success"
+  retryCount=120
+  retries=0
+  check_for_csv_success=$(oc get csv -n "$projectName" --ignore-not-found | grep --color=never "${operatorName}" | awk -F' ' '{print $NF}')
+  until [[ $retries -eq $retryCount || $check_for_csv_success = "Succeeded" ]]; do
+    sleep 5
+    check_for_csv_success=$(oc get csv -n "$projectName" --ignore-not-found | grep --color=never "${operatorName}" | awk -F' ' '{print $NF}')
+    retries=$((retries + 1))
+  done
+  echo "$check_for_csv_success"
 }
 
-function createProject(){
+function createProject() {
   existingns=$(oc get projects | grep -w "${projectName}" | awk '{print $1}')
 
   if [ "${existingns}" == "${projectName}" ]; then
-    echoYellow "Project ${existingns} already exists, do you want to continue operator installation in the existing project? [Y/n]: "
-    read -r continueInstall </dev/tty
-    if [[ ! $continueInstall || $continueInstall = *[^Yy] ]]; then
-      echoRed "Aborting installation of Operator."
-      exit 0;
-    fi
+    echoYellow "Project ${existingns} already exists."
   else
     oc new-project "${projectName}" &>>"${logFile}"
-      if [ $? -ne 0 ];then
-	echoRed "FAILED: Project:${projectName} creation failed"
-	exit 1
-     fi
+    if [ $? -ne 0 ]; then
+      echoRed "FAILED: Project:${projectName} creation failed"
+      exit 1
+    fi
   fi
 }
